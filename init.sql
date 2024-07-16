f45aa48541c95116166dbf9068802192e5165b0a
magali <maguimourino@gmail.com>
2024-07-15 22:02:53 -0300

    Renamed tables to singular and updated some fields.

--- init.sql
+++ init.sql
@@ -1,38 +1,38 @@
 USE hospital_db;
 
-CREATE TABLE patients (
+CREATE TABLE patient (
   id INT AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(100) UNIQUE NOT NULL,
-  'password' VARCHAR(100) NOT NULL,
+  password VARCHAR(100) NOT NULL,
   first_name VARCHAR(100),
   last_name VARCHAR(100),
   birthdate DATE,
-  email VARCHAR(255),
+  email VARCHAR(100),
   phone VARCHAR(20)
 );
 
-CREATE TABLE doctors (
+CREATE TABLE doctor (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  `name` VARCHAR(100),
+  name VARCHAR(100),
   specialty VARCHAR(100)
 );
 
-CREATE TABLE schedules (
+CREATE TABLE schedule(
   id INT AUTO_INCREMENT PRIMARY KEY,
   doctor_id INT,
-  `date` DATE,
+  day_of_week VARCHAR(10),
   start_time TIME,
   end_time TIME,
-  FOREIGN KEY (doctor_id) REFERENCES doctors(id)
+  FOREIGN KEY (doctor_id) REFERENCES doctor(id)
 );
 
-CREATE TABLE appointments (
+CREATE TABLE appointment (
   id INT AUTO_INCREMENT PRIMARY KEY,
   patient_id INT,
   doctor_id INT,
-  `date` DATE,
-  `time` TIME,
+  date DATE,
+  time TIME,
   duration INT,
-  FOREIGN KEY (patient_id) REFERENCES patients(id),
-  FOREIGN KEY (doctor_id) REFERENCES doctors(id)
+  FOREIGN KEY (patient_id) REFERENCES patient(id),
+  FOREIGN KEY (doctor_id) REFERENCES doctor(id)
 );
--- src/models/patientsModel.js
+++ src/models/patientsModel.js
@@ -1,22 +1,41 @@
 const db = require('./db');
 
-exports.getAllPatients = (callback) => {
-    db.query('SELECT * FROM patients', callback);
-};
-
 exports.createPatient = async (patient) => {
     const { username, password, firstName, lastName, email, phone, birthdate } = patient;
     await db.query(
-      'INSERT INTO patients (username, password, first_name, last_name, email, phone, birthdate) VALUES (?, ?, ?, ?, ?, ?, ?)',
+      'INSERT INTO patient (username, password, first_name, last_name, email, phone, birthdate) VALUES (?, ?, ?, ?, ?, ?, ?)',
       [username, password, firstName, lastName, email, phone, birthdate]
     );
 };
 
 exports.findPatientByUsername = async (username) => {
-    const [result] = await db.query('SELECT * FROM patients WHERE username = ?', [username]);
+    const [result] = await db.query('SELECT * FROM patient WHERE username = ?', [username]);
     return result[0];
 };
 
 exports.getPatientById = (id, callback) => {
-    db.query('SELECT * FROM patients WHERE id = ?', [id], callback);
+    db.query('SELECT * FROM patient WHERE id = ?', [id], callback);
+};
+
+exports.updatePatient = async (patientId, patientData) => {
+    const { first_name, last_name, email, phone, birthdate } = patientData;
+
+    const query = `
+        UPDATE patient 
+        SET 
+            first_name = ?, 
+            last_name = ?, 
+            email = ?, 
+            phone = ?, 
+            birthdate = ?
+        WHERE id = ?;
+    `;
+
+    const values = [first_name, last_name, email, phone, birthdate, patientId];
+
+    try {
+        await db.query(query, values);
+    } catch (err) {
+        throw new Error('Error updating patient information: ' + err.message);
+    }
 };
