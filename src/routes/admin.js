b14a512a09c53b11dc928edb66f6c5cca192a855
magali <maguimourino@gmail.com>
2024-07-30 21:26:40 -0300

    .

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const patientController = require('../controllers/patientsController');
-const doctorsController = require('../controllers/doctorController');
 const authenticate = require('../middleware/auth');
 const { isAdmin } = require('../middleware/role');
 
